b9d0a01bf060dde94f0324b6e09b3fa7a684c23f
krish dhiman <krishdhiman86@gmail.com>
2026-08-05 21:22:27 +0530

    find maximum element

---
+++ Arrays/MaxElement.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class MaxElement {
+    public static void main() {
+        int[] arr = {10, 80, 30, 100, 40};
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("maximum element found:"+ max);
+    }
+}
