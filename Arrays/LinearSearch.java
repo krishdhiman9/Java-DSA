891fcac733ba374716e5924d15827e0e46de6e4f
krish dhiman <krishdhiman86@gmail.com>
2026-08-06 17:03:10 +0530

    linear search program

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class LinearSearch {
+    public static void main() {
+        int[] arr = {10, 20, 30, 40, 50};
+        int num = 3;
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (num == arr[i]) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("element found");
+        } else {
+            System.out.println("element not found");
+        }
+    }
+}
