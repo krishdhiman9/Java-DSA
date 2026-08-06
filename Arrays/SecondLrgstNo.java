a62ba400a53f0f52a098bcf8a21cbc7b4a49bf67
krish dhiman <krishdhiman86@gmail.com>
2026-08-06 21:24:56 +0530

    2nd largest number program

---
+++ Arrays/SecondLrgstNo.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class SecondLrgstNo {
+    public static void main() {
+        int []arr = {5, 100, 80, 70, 60};
+        int largest = arr[0];
+        int scndlargst = arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(largest<arr[i]){
+                scndlargst = largest;
+                largest = arr[i];
+            }
+            else if(scndlargst<arr[i]){
+                scndlargst = arr[i];
+            }
+        }
+        System.out.println("largest element found "+ largest);
+        System.out.println("second largest elementbfound "+ scndlargst);
+    }
+}
