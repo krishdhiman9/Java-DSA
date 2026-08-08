f5ef8b6ce0c8a755447cd6a50c98e5dcbf0adf64
krish dhiman <krishdhiman86@gmail.com>
2026-08-08 16:16:50 +0530

    count positive and negative number

---
+++ Arrays/countPositiveNegative.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class countPositiveNegative {
+    static void main() {
+        int[] arr = {10, -5, 20, -8, 0, 15, -2};
+        int countPositive = 0;
+        int countNegative = 0;
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]>0){
+                countPositive++;
+            }
+            else if(arr[i]<0){
+                countNegative++;
+            }
+        }
+        System.out.println("positive number is "+ countPositive);
+        System.out.println("negative number is "+ countNegative);
+
+    }
+}
