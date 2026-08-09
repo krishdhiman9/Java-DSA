ac8d39eeebe8529a95bb85a52ac180bbbd80622b
krish dhiman <krishdhiman86@gmail.com>
2026-08-09 17:13:10 +0530

    remove extra spaces

--- Arrays/FindDuplicateElement.java
+++ Arrays/FindDuplicateElement.java
@@ -1,9 +1,9 @@
 package Arrays;
-
 public class FindDuplicateElement {
     static void main() {
         int[] arr = {10, 20, 30, 30, 40};
         int repeate = 0;
+
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
                 if (arr[i] == arr[j]) {
@@ -13,7 +13,5 @@ public class FindDuplicateElement {
             }
         }
         System.out.println(repeate);
-
-
     }
 }
