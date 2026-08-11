035686ebe406f7cbc1cef61c56f4d4ae7d4bce5f
krish dhiman <krishdhiman86@gmail.com>
2026-08-11 14:43:43 +0530

    binary search (find element in array)

---
+++ Arrays/binarySearch.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class binarySearch {
+    static void main() {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int target = 50;
+
+        int left = 0;
+        int right = arr.length - 1;
+        while (left<=right) {
+            int mid = (left + right) / 2;
+            if (target == arr[mid]) {
+                System.out.println("Element found at index: "+ mid);
+                return;
+            }
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("element not found");
+    }
+}
