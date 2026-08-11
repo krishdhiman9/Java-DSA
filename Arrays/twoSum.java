52e9d85849e172b05719f1b23e610e84fd474eb9
krish dhiman <krishdhiman86@gmail.com>
2026-08-11 13:51:32 +0530

    two sum program

---
+++ Arrays/twoSum.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class twoSum {
+    static void main() {
+        int[] arr = {2, 7, 11, 15};
+        int target = 18;
+        //int sum=0;
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+}
