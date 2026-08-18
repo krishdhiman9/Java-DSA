a9d6bba1a60a3770121d2e8ef9f92cf03b9aa4a6
krish dhiman <krishdhiman86@gmail.com>
2026-08-18 20:17:18 +0530

    bubble sort program

--- Arrays/sorting/bubbleSort.java
+++ Arrays/sorting/bubbleSort.java
@@ -2,23 +2,21 @@ package Arrays.sorting;
 
 public class bubbleSort {
     static void main() {
-        int[] arr = {5, 3, 8, 1, 2, 7};
+        int [] arr = {10,3,5,2,1,6,7,8,4};
+        int tem = 0;
 
-        for (int i = 0; i < arr.length - 1; i++) {
-
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-
-                if (arr[j] > arr[j + 1]) {
-
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+        for(int i = 0; i<arr.length-1; i++){
+            for (int j=0; j <arr.length-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    tem=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tem;
                 }
             }
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for (int i=0; i<=arr.length-1; i++){
+            System.out.print(arr[i]+ " ");
         }
+
     }
 }
