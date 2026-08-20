5c9c106fe5120dd342f9a0d426c2c56f2ed0cb0e
krish dhiman <krishdhiman86@gmail.com>
2026-08-20 22:52:20 +0530

    selection sort program

---
+++ Arrays/sorting/SelectionSort.java
@@ -0,0 +1,25 @@
+package Arrays.sorting;
+
+public class SelectionSort {
+    static void main() {
+        int [] arr = {7, 4, 9, 2, 5};
+        int min = 0;
+
+        for(int i =0; i<arr.length-1; i++){
+            min = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j]<arr[min]){
+                    min= j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min]=temp;
+        }
+        for(int i=0; i<arr.length; i++) {
+
+
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
