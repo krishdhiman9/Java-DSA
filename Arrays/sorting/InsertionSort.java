2560e96be08e6ecf45117ca9b7cb757ed5ca6ef2
krish dhiman <krishdhiman86@gmail.com>
2026-08-21 22:03:33 +0530

    Create InsertionSort class

---
+++ Arrays/sorting/InsertionSort.java
@@ -0,0 +1,12 @@
+package Arrays.sorting;
+
+public class InsertionSort {
+    static void main() {
+        int [] arr = {7,4,9,2,5};
+        int sorted = arr[0];
+
+        for (int i=1; i < arr.length; i++){
+
+        }
+    }
+}
