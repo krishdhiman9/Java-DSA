b713af8d1b375775a6167b181c93089a3d47c868
krish dhiman <krishdhiman86@gmail.com>
2026-08-22 22:38:10 +0530

    insertion sort program

--- Arrays/sorting/InsertionSort.java
+++ Arrays/sorting/InsertionSort.java
@@ -2,11 +2,20 @@ package Arrays.sorting;
 
 public class InsertionSort {
     static void main() {
-        int [] arr = {7,4,9,2,5};
-        int sorted = arr[0];
+        int[] arr = {7, 4, 9, 2, 5};
 
-        for (int i=1; i < arr.length; i++){
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
 
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        for(int i =0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
         }
     }
 }
