a562443ff20ab0e0e61c8a4af7c937a1583637d3
krish dhiman <krishdhiman86@gmail.com>
2026-08-24 22:57:02 +0530

    move zeroes

--- Arrays/TwoPointer/moveZeroes.java
+++ Arrays/TwoPointer/moveZeroes.java
@@ -1,22 +1,56 @@
 package Arrays.TwoPointer;
 public class moveZeroes {
-    static void main() {
-        int[] arr = {0, 1, 0, 3, 0, 12};
-        int index = 0;
+//    static void main() {
+//        int[] arr = {0, 1, 0, 3, 0, 12};
+//        int index = 0;
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] != 0) {
+//                arr[index] = arr[i];
+//                index++;
+//            }
+//        }
+//
+//        for(int i= index; i<arr.length; i++){
+//            arr[i]=0;
+//        }
+//
+//        for(int i=0; i<arr.length; i++){
+//            System.out.print(arr[i]+ " ");
+//        }
+//    }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                arr[index] = arr[i];
-                index++;
-            }
-        }
 
-        for(int i= index; i<arr.length; i++){
-            arr[i]=0;
-        }
+public static void main() {
+    int[] arr = {0, 1, 0, 3, 0, 12};
+    int index = 0;
 
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+ " ");
+    // Count zeroes
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == 0) {
+            index++;
         }
     }
+
+    // Put non-zero elements after zeroes
+    int j = index;
+
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+
+    // Put zeroes at beginning
+    for (int i = 0; i < index; i++) {
+        arr[i] = 0;
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+    }
 }
+
+}
+
