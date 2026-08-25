6fff542144941af08c2dc7abad07db3a1310f722
krish dhiman <krishdhiman86@gmail.com>
2026-08-25 22:42:33 +0530

    divide array in merge sort

--- Arrays/sorting/MergeSort.java
+++ Arrays/sorting/MergeSort.java
@@ -2,6 +2,32 @@ package Arrays.sorting;
 
 public class MergeSort {
     static void main() {
+        int[] arr = {7, 4, 9, 2, 5};
 
-    }
-}
+                int mid = arr.length / 2;
+
+                int[] left = new int[mid];
+                int[] right = new int[arr.length - mid];
+
+                for (int i = 0; i < mid; i++) {
+                    left[i] = arr[i];
+                }
+
+                for (int i = mid; i < arr.length; i++) {
+                    right[i - mid] = arr[i];
+                }
+
+                System.out.println("Left array:");
+                for (int i = 0; i < left.length; i++) {
+                    System.out.print(left[i] + " ");
+                }
+
+                System.out.println();
+
+                System.out.println("Right array:");
+                for (int i = 0; i < right.length; i++) {
+                    System.out.print(right[i] + " ");
+                }
+            }
+        }
+
