3a0c1075ee7e5d3e533d9c4a6b33dbb46c3a9c8a
krish dhiman <krishdhiman86@gmail.com>
2026-08-25 22:28:12 +0530

    Format array code

--- Arrays/basicArray/twoSum.java
+++ Arrays/basicArray/twoSum.java
@@ -4,7 +4,6 @@ public class twoSum {
     static void main() {
         int[] arr = {2, 7, 11, 15};
         int target = 18;
-        //int sum=0;
 
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
