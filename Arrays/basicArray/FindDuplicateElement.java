e383830369ce158a9119bc726c543a42c70b7cb3
krish dhiman <krishdhiman86@gmail.com>
2026-08-28 16:10:03 +0530

    Brute force method

--- Arrays/basicArray/FindDuplicateElement.java
+++ Arrays/basicArray/FindDuplicateElement.java
@@ -1,18 +1,27 @@
 package Arrays.basicArray;
-public class FindDuplicateElement {
-    static void main() {
-        int[] arr = {10, 20, 40, 30, 40};
-        int repeate = 0;
+//Brute Force method
 
-        for(int i=0; i<arr.length; i++){
-            for(int j=i+1; j<arr.length; j++){
-                if (arr[i] == arr[j]) {
-                    repeate = arr[i];
-                    break;
-                }
-            }
-        }
+//public class FindDuplicateElement {
+//    static void main() {
+//        int[] arr = {10, 20, 40, 30, 40};
+//        int repeate = 0;
+//
+//        for(int i=0; i<arr.length; i++){
+//            for(int j=i+1; j<arr.length; j++){
+//                if (arr[i] == arr[j]) {
+//                    repeate = arr[i];
+//                    break;
+//                }
+//            }
+//        }
+//
+//        System.out.println(repeate);
+//    }
+//}
 
-        System.out.println(repeate);
-    }
+//Floyd's Cycle Detection Algorithm / Slow & Fast Pointer approach
+public class FindDuplicateElement{
+static void main() {
+
+}
 }
