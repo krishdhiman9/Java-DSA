4737735a7d8e2988590b2be244e0a50ede2aaeda
krish dhiman <krishdhiman86@gmail.com>
2026-08-30 14:06:42 +0530

    code updated

--- Arrays/basicArray/plusOne.java
+++ Arrays/basicArray/plusOne.java
@@ -1,11 +1,23 @@
 package Arrays.basicArray;
+import java.util.Arrays;
 
 public class plusOne {
     static void main() {
-        int[] arr = {1,2,3};
+        int[] arr = {1, 2, 9};
+        int[] newArr= new int[arr.length+1];
 
-        for (int i =arr.length-1; i>=0; i++){
+        for (int i =arr.length-1; i>=0; i--){
+            if(arr[i]==9){
+                arr[i]=0;
 
+            }
+            else {
+                arr[i] = arr[i]+1;
+                break;
+            }
+            newArr[i]=arr[i];
         }
+        newArr[0]=newArr[0]+1;
+        System.out.println(Arrays.toString(newArr));
     }
 }
