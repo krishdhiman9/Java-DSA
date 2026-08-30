f969a164fa20df5a2e7d56fc2e32f4d027ef553f
krish dhiman <krishdhiman86@gmail.com>
2026-08-30 15:00:24 +0530

    one plus program completed

--- Arrays/basicArray/plusOne.java
+++ Arrays/basicArray/plusOne.java
@@ -3,21 +3,26 @@ import java.util.Arrays;
 
 public class plusOne {
     static void main() {
-        int[] arr = {1, 2, 9};
-        boolean create = false;
+        int[] arr = {9, 2, 9};
+        boolean allNine = true;
         for (int i =arr.length-1; i>=0; i--){
             if(arr[i]==9){
                 arr[i]=0;
             }
             else {
                 arr[i] = arr[i]+1;
+                allNine =false;
                 break;
             }
         }
 
-        if
-
-        newArr[0]=newArr[0]+1;
-        System.out.println(Arrays.toString(arr));
+        if(allNine) {
+            int[] newArr = new int[arr.length + 1];
+            newArr[0] = newArr[0] + 1;
+            System.out.println(Arrays.toString(newArr));
+        }
+            else{
+                System.out.println(Arrays.toString(arr));
+            }
     }
 }
