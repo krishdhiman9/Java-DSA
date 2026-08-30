addfc971353676e15a9c65b8831f6ef72f2c16c9
krish dhiman <krishdhiman86@gmail.com>
2026-08-30 14:21:08 +0530

    add if-else condition

--- Arrays/basicArray/plusOne.java
+++ Arrays/basicArray/plusOne.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 public class plusOne {
     static void main() {
         int[] arr = {1, 2, 9};
-        int[] newArr= new int[arr.length+1];
-
+        boolean create = false;
         for (int i =arr.length-1; i>=0; i--){
             if(arr[i]==9){
                 arr[i]=0;
-
             }
             else {
                 arr[i] = arr[i]+1;
                 break;
             }
-            newArr[i]=arr[i];
         }
+
+        if
+
         newArr[0]=newArr[0]+1;
-        System.out.println(Arrays.toString(newArr));
+        System.out.println(Arrays.toString(arr));
     }
 }
