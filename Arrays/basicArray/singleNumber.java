e2a905da413c7fda4b7e8881683cf8b986ad263c
krish dhiman <krishdhiman86@gmail.com>
2026-08-31 14:23:38 +0530

    Add comment

--- Arrays/basicArray/singleNumber.java
+++ Arrays/basicArray/singleNumber.java
@@ -6,6 +6,8 @@ public class singleNumber {
         int result=0;
 
         for (int i=0; i<arr.length; i++){
+            // XOR operator is used to cancel out duplicate elements
+            // and return the element that appears only once.
             result = result ^ arr[i];
 
         }
