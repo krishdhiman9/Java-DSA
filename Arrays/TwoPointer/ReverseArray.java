16c9ec7d5fcec6079fe4f529172631e9aeb14113
krish dhiman <krishdhiman86@gmail.com>
2026-09-03 11:23:25 +0530

    update elements

--- Arrays/TwoPointer/ReverseArray.java
+++ Arrays/TwoPointer/ReverseArray.java
@@ -4,7 +4,7 @@ package Arrays.TwoPointer;
 
             public static void main(String[] args) {
 
-                int[] arr = {10, 20, 30, 40, 50};
+                int[] arr = {10, 20, 30, 40, 50, 60};
 
                 int left = 0;
                 int right = arr.length - 1;
