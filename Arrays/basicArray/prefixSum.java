d6f2a89ff8a794b217c2be2fda5c99313d661b7d
krish dhiman <krishdhiman86@gmail.com>
2026-09-03 11:05:00 +0530

    prefix program

--- Arrays/basicArray/prefixSum.java
+++ Arrays/basicArray/prefixSum.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class prefixSum {
     static void main() {
         int [] arr={2, 4, 1, 3, 5};
-
         for(int i=1; i<arr.length; i++){
              arr[i] = arr[i-1]+arr[i];
         }
