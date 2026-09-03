78115394933948e5bfd8c2c8605ba8638e37236e
krish dhiman <krishdhiman86@gmail.com>
2026-09-03 10:53:10 +0530

    add condition

--- Arrays/basicArray/prefixSum.java
+++ Arrays/basicArray/prefixSum.java
@@ -3,11 +3,9 @@ import java.util.Arrays;
 public class prefixSum {
     static void main() {
         int [] arr={2, 4, 1, 3, 5};
-        int sum = 0;
 
-        for(int i=0; i<arr.length-1; i++){
-             arr[i] = arr[i+1]+arr[i];
-             arr[i] = sum;
+        for(int i=1; i<arr.length; i++){
+             arr[i] = arr[i-1]+arr[i];
         }
         System.out.println(Arrays.toString(arr));
     }
