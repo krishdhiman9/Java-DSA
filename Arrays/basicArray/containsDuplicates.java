0c75658352140c8b4cb2e9d041e6945732fd22b8
krish dhiman <krishdhiman86@gmail.com>
2026-09-04 22:36:13 +0530

    create class contains duplicates

---
+++ Arrays/basicArray/containsDuplicates.java
@@ -0,0 +1,19 @@
+package Arrays.basicArray;
+
+public class containsDuplicates {
+    static void main() {
+        int [] nums = {1,2,3,1};
+    boolean result=false;
+
+    for(int i=0; i<nums.length-1; i++){
+        for(int j =i+1; j<nums.length; j++){
+            if(nums[i] ==nums[j]){
+                return true;
+            }
+        }
+    }
+
+ return result;
+    }
+}
+
