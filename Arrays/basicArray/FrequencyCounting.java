5b864b68a02d9de6fd36cedbd61d811e42930993
krish dhiman <krishdhiman86@gmail.com>
2026-09-06 16:05:21 +0530

    FrequencyCounting program

--- Arrays/basicArray/FrequencyCounting.java
+++ Arrays/basicArray/FrequencyCounting.java
@@ -4,7 +4,12 @@ public class FrequencyCounting {
     static void main() {
         int[] arr = {1, 2, 3, 2, 4, 2, 5};
         int target = 2;
-
-        for(int i)
+        int count =0;
+        for(int i=0; i<arr.length; i++){
+            if(target == arr[i]){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
