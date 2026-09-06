56b55bf120812ad2ac89ebb6265f8024fea14b5a
krish dhiman <krishdhiman86@gmail.com>
2026-09-06 16:02:30 +0530

    create class

---
+++ Arrays/basicArray/FrequencyCounting.java
@@ -0,0 +1,10 @@
+package Arrays.basicArray;
+
+public class FrequencyCounting {
+    static void main() {
+        int[] arr = {1, 2, 3, 2, 4, 2, 5};
+        int target = 2;
+
+        for(int i)
+    }
+}
