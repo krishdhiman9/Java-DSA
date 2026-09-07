cb8e41c3aca273b41bd71feaf07c7ce06c0988c4
krish dhiman <krishdhiman86@gmail.com>
2026-09-07 13:24:41 +0530

    FrequencyCounting program

--- Arrays/basicArray/FrequencyCounting.java
+++ Arrays/basicArray/FrequencyCounting.java
@@ -2,14 +2,35 @@ package Arrays.basicArray;
 
 public class FrequencyCounting {
     static void main() {
-        int[] arr = {1, 2, 3, 2, 4, 2, 5};
-        int target = 2;
-        int count =0;
-        for(int i=0; i<arr.length; i++){
-            if(target == arr[i]){
-                count++;
+
+        int[] arr = {1, 2, 2, 3, 1, 2, 4, 3};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            boolean alreadyCounted = false;
+
+            for (int k = 0; k < i; k++) {
+
+                if (arr[i] == arr[k]) {
+                    alreadyCounted = true;
+                    break;
+                }
             }
+
+            if (alreadyCounted) {
+                continue;
+            }
+
+            int count = 0;
+
+            for (int j = i; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            System.out.println(arr[i] + " is occurring " + count + " times");
         }
-        System.out.println(count);
     }
 }
