34174dfac596452d880f2a35fd03c055ac253e06
krish dhiman <krishdhiman86@gmail.com>
2026-09-08 22:07:49 +0530

    import hashset

--- Arrays/basicArray/containsDuplicates.java
+++ Arrays/basicArray/containsDuplicates.java
@@ -1,18 +1,34 @@
 package Arrays.basicArray;
+import java.util.HashSet;
 
-public class containsDuplicates {
-    static void  main() {
-        int [] nums = {1,2,2,2,3,1};
-    boolean result=false;
+//public class containsDuplicates {
+//    static void  main() {
+//        int [] nums = {1,2,2,2,3,1};
+//    boolean result=false;
+//
+//    for(int i=0; i<nums.length-1; i++){
+//        for(int j =i+1; j<nums.length; j++){
+//            if(nums[i] ==nums[j]){
+//                result = true;
+//            }
+//        }
+//    }
+//        System.out.println( result);
+//    }
+//}
 
-    for(int i=0; i<nums.length-1; i++){
-        for(int j =i+1; j<nums.length; j++){
-            if(nums[i] ==nums[j]){
-                result = true;
+public class  containsDuplicates{
+    public boolean containsDuplicate() {
+int [] num = {1,2,2,2,3,1};
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
             }
+            set.add(num);
         }
-    }
-        System.out.println( result);
+
+        return false;
     }
 }
-
