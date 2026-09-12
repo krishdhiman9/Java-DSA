87493105bb443f2ff655c1b2f390d007ed01dd52
krish dhiman <krishdhiman86@gmail.com>
2026-09-12 15:02:23 +0530

    move zeroes at starting program

--- Arrays/TwoPointer/movesZeroesStarting.java
+++ Arrays/TwoPointer/movesZeroesStarting.java
@@ -1,9 +1,35 @@
 package Arrays.TwoPointer;
 
+import java.util.Arrays;
+
 public class movesZeroesStarting {
     static void main() {
-        int [] arr = {0, 1, 0, 3, 0, 12};
+        int[] arr = {0, 1, 0, 3, 0, 12};
+        int index = 0;
 
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                index++;
+            }
+        }
 
+        int nonZero = index;
+
+        // Original array ko overwrite nahi karna
+        int[] temp = new int[arr.length];
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != 0) {
+                temp[nonZero] = arr[j];
+                nonZero++;
+            }
+        }
+
+        // Zeroes beginning mein
+        for (int i = 0; i < index; i++) {
+            temp[i] = 0;
+        }
+
+        System.out.println(Arrays.toString(temp));
     }
 }
