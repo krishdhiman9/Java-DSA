4dd2ae925b0e60519ecd43a6effe4819e5ed07f1
krish dhiman <krishdhiman86@gmail.com>
2026-09-16 11:32:19 +0530

    import arrays library

--- Arrays/basicArray/removeDuplicate.java
+++ Arrays/basicArray/removeDuplicate.java
@@ -1,7 +1,15 @@
 package Arrays.basicArray;
-
+import java.util.Arrays;
 public class removeDuplicate {
     static void main() {
+        int [] arr = {0, 0, 1, 1, 2, 2, 3};
+        int k = 0;
 
+        for(int i =0; i<arr.length; i++){
+            if (arr[i]!=arr[i-1]){
+                arr[k]=arr[i];
+                k++;
+            }
+        }
     }
 }
