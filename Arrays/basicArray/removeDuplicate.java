907ea887d00258ba78a39a0167e0749c19d8e513
krish dhiman <krishdhiman86@gmail.com>
2026-09-16 11:41:14 +0530

    remove duplicates from the array

--- Arrays/basicArray/removeDuplicate.java
+++ Arrays/basicArray/removeDuplicate.java
@@ -1,15 +1,18 @@
 package Arrays.basicArray;
 import java.util.Arrays;
 public class removeDuplicate {
-    static void main() {
+    public static void main(String[] args) {
         int [] arr = {0, 0, 1, 1, 2, 2, 3};
-        int k = 0;
+        int k = 1;
 
-        for(int i =0; i<arr.length; i++){
+        for(int i =1; i<arr.length; i++){
             if (arr[i]!=arr[i-1]){
                 arr[k]=arr[i];
                 k++;
             }
+        }
+        for(int i=0; i<k; i++){
+            System.out.print(arr[i] + " ");
         }
     }
 }
