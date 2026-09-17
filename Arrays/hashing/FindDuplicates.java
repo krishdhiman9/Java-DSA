231ad924940180bb1168c7f2699e2f3f5f5caea8
krish dhiman <krishdhiman86@gmail.com>
2026-09-17 14:17:06 +0530

    find duplicate with hashing

--- Arrays/hashing/FindDuplicates.java
+++ Arrays/hashing/FindDuplicates.java
@@ -2,12 +2,14 @@ package Arrays.hashing;
 import java.util.HashSet;
 public class FindDuplicates {
     static void main() {
-        int[] arr = {1, 2, 3, 4, 2,};
+        int[] arr = {1, 2, 3, 4, 3};
         HashSet<Integer> set = new HashSet<>();
         for (int num : arr) {
-            if (set.contains(arr)) {
-
+            if (set.contains(num)) {
+                System.out.println(num);
             }
+            set.add(num);
         }
+        System.out.println("no duplicate");
     }
 }
