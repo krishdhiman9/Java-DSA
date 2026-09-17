21745646a113e9892aba9d431462a5da419c1009
krish dhiman <krishdhiman86@gmail.com>
2026-09-17 14:24:56 +0530

    make clean code

--- Arrays/hashing/FindDuplicates.java
+++ Arrays/hashing/FindDuplicates.java
@@ -1,9 +1,12 @@
 package Arrays.hashing;
 import java.util.HashSet;
 public class FindDuplicates {
+
     static void main() {
-        int[] arr = {1, 2, 3, 4, 3};
+
         HashSet<Integer> set = new HashSet<>();
+
+        int[] arr = {1, 2, 3, 4, 3};
         for (int num : arr) {
             if (set.contains(num)) {
                 System.out.println(num);
