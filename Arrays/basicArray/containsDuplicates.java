c3bacba44fabc6353569bf6301a2f918a347b2f4
krish dhiman <krishdhiman86@gmail.com>
2026-09-17 14:04:03 +0530

    contains duplicates with Brute force

--- Arrays/basicArray/containsDuplicates.java
+++ Arrays/basicArray/containsDuplicates.java
@@ -1,46 +1,44 @@
 package Arrays.basicArray;
-import java.util.HashSet;
-
-//public class containsDuplicates {
-//    static void  main() {
-//        int [] nums = {1,2,2,2,3,1};
-//    boolean result=false;
-//
-//    for(int i=0; i<nums.length-1; i++){
-//        for(int j =i+1; j<nums.length; j++){
-//            if(nums[i] ==nums[j]){
-//                result = true;
-//            }
-//        }
-//    }
-//        System.out.println( result);
-//    }
-//}
-
-
-
-import java.util.HashSet;
-
 public class containsDuplicates {
+    static void  main() {
+        int [] nums = {1,2,2,2,3,1};
+    boolean result=false;
 
-    public static void main(String[] args) {
-
-        int[] nums = {1, 2, 2, 2, 3, 1};
-
-        boolean result = false;
-
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int num : nums) {
-
-            if (set.contains(num)) {
+    for(int i=0; i<nums.length-1; i++){
+        for(int j =i+1; j<nums.length; j++){
+            if(nums[i] ==nums[j]){
                 result = true;
-                break;
             }
-
-            set.add(num);
         }
-
-        System.out.println(result);
+    }
+        System.out.println( result);
     }
 }
+
+
+
+//import java.util.HashSet;
+
+//public class containsDuplicates {
+//
+//    public static void main(String[] args) {
+//
+//        int[] nums = {1, 2, 2, 2, 3, 1};
+//
+//        boolean result = false;
+//
+//        HashSet<Integer> set = new HashSet<>();
+//
+//        for (int num : nums) {
+//
+//            if (set.contains(num)) {
+//                result = true;
+//                break;
+//            }
+//
+//            set.add(num);
+//        }
+//
+//        System.out.println(result);
+//    }
+//}
