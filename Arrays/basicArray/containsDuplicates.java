4673f65e26c6b59b65f28a7ea8608a5cd3b331b5
krish dhiman <krishdhiman86@gmail.com>
2026-09-17 14:25:48 +0530

    remove extra spaces

--- Arrays/basicArray/containsDuplicates.java
+++ Arrays/basicArray/containsDuplicates.java
@@ -14,31 +14,3 @@ public class containsDuplicates {
         System.out.println( result);
     }
 }
-
-
-
-//import java.util.HashSet;
-
-//public class containsDuplicates {
-//
-//    public static void main(String[] args) {
-//
-//        int[] nums = {1, 2, 2, 2, 3, 1};
-//
-//        boolean result = false;
-//
-//        HashSet<Integer> set = new HashSet<>();
-//
-//        for (int num : nums) {
-//
-//            if (set.contains(num)) {
-//                result = true;
-//                break;
-//            }
-//
-//            set.add(num);
-//        }
-//
-//        System.out.println(result);
-//    }
-//}
