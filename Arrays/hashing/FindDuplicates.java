50d40d44b2575af8506f6f3f708dd7608389428f
krish dhiman <krishdhiman86@gmail.com>
2026-09-17 14:12:11 +0530

    create class

---
+++ Arrays/hashing/FindDuplicates.java
@@ -0,0 +1,13 @@
+package Arrays.hashing;
+import java.util.HashSet;
+public class FindDuplicates {
+    static void main() {
+        int[] arr = {1, 2, 3, 4, 2,};
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(arr)) {
+
+            }
+        }
+    }
+}
