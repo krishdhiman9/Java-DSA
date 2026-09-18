ff54c2590f9c18aece730dbf57643118810a7ed1
krish dhiman <krishdhiman86@gmail.com>
2026-09-18 21:55:18 +0530

    find duplicate program

--- Arrays/hashing/FindDuplicates.java
+++ Arrays/hashing/FindDuplicates.java
@@ -5,7 +5,7 @@ public class FindDuplicates {
     static void main() {
 
         HashSet<Integer> set = new HashSet<>();
-        int[] arr = {1, 2, 3, 4, 3};
+        int[] arr = {1, 2, 3, 4, 3,5,3};
         for (int num : arr) {
             if (set.contains(num)) {
                 System.out.println(num);
