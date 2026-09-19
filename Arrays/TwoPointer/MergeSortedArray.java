24f91b1a72ee4987792a5bc355c6d9f68d177937
krish dhiman <krishdhiman86@gmail.com>
2026-09-19 22:42:09 +0530

    Merge sorted array program

--- Arrays/TwoPointer/MergeSortedArray.java
+++ Arrays/TwoPointer/MergeSortedArray.java
@@ -1,19 +1,30 @@
 package Arrays.TwoPointer;
-
+import java.util.Arrays;
 public class MergeSortedArray {
     static void main() {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {2, 5, 6};
+        int[] nums1 = {1, 2, 3, 0, 0, 0}; //i
+        int[] nums2 = {2, 5, 6}; //j
 
         int m = 3;
         int n = 3;
 
         int i = m-1;
         int j = n-1;
-        int k = nums2.length-1;
+        int k = nums1.length-1;
 
 
-        while()
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
+        }
+        System.out.println(Arrays.toString(nums1));
 
     }
 }
