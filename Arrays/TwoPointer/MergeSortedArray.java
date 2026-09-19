dec0b83f91e403e0445ef293ad9b75d104995f94
krish dhiman <krishdhiman86@gmail.com>
2026-09-19 22:32:12 +0530

    create class

---
+++ Arrays/TwoPointer/MergeSortedArray.java
@@ -0,0 +1,16 @@
+package Arrays.TwoPointer;
+
+public class MergeSortedArray {
+    static void main() {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+
+        int m = 3;
+        int n = 3;
+
+        int
+
+        while()
+
+    }
+}
