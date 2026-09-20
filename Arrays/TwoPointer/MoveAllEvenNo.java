a4e67f98531a82df7ed49f16c87b0a3c39a007a8
krish dhiman <krishdhiman86@gmail.com>
2026-09-20 14:50:22 +0530

    move all even numbers in front

--- Arrays/TwoPointer/MoveAllEvenNo.java
+++ Arrays/TwoPointer/MoveAllEvenNo.java
@@ -3,6 +3,17 @@ import java.util.Arrays;
 
 public class MoveAllEvenNo {
     static void main() {
+        int [] arr={3, 8, 5, 2, 7, 4};
+        int slow = 0;
+        int fast = 0;
 
+        for (int i =0; i<arr.length; i++) {
+            if (arr[fast] % 2 == 0) {
+                arr[slow] = arr[fast];
+                slow++;
+            }
+            fast++;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
