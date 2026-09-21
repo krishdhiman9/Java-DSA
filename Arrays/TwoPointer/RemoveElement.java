fde27ef9e43701ccd9f3ed13ea7b301773425225
krish dhiman <krishdhiman86@gmail.com>
2026-09-21 22:01:50 +0530

    remove extra spaces

--- Arrays/TwoPointer/RemoveElement.java
+++ Arrays/TwoPointer/RemoveElement.java
@@ -5,7 +5,6 @@ public class RemoveElement {
         int [] a = {0, 1, 2, 2, 3, 0, 4, 2};
         int val = 2;
         int k = 0;
-
         for(int i = 0; i<a.length; i++){
             if(val!=a[i]){
                 a[k]=a[i];
