41a1a602abd8215abf6788c1aac215fb826c76ce
krish dhiman <krishdhiman86@gmail.com>
2026-09-25 14:40:14 +0530

    ContainerWithMostWater program

--- Arrays/TwoPointer/ContainerWithMostWater.java
+++ Arrays/TwoPointer/ContainerWithMostWater.java
@@ -1,22 +1,31 @@
 package Arrays.TwoPointer;
 
+import Arrays.basicArray.MaxElement;
+
+import static java.lang.Math.max;
+
 public class ContainerWithMostWater {
     static void main() {
         int [] c = {1, 8, 6, 2, 5, 4, 8, 3, 7};
         int left = 0;
         int right = c.length -1;
-        int width = 0;
-        //int max = 0 ;
+        int max = 0 ;
 
-        while(left<right){
+        while(left<right) {
+            int w = right - left;
+            int h = Math.min(c[left], c[right]);
+            int currentwater = w * h;
+            max = max(currentwater, max);
+
             if(c[left]<c[right]){
-                width =right-left;
-                int max = left*width;
                 left++;
             }
-            else{
+            else {
                 right--;
             }
+
         }
+        System.out.println(max);
     }
 }
+
